ff8e214db3af7be5971eb531b366dc0309fab4f0
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-01-22 19:50:04 -0300

    feat: adicionando resposta em json e condicional para a resposta

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require("http");
 
+var message = ""
+
 http
-  .createServer((request, response) => {})
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    if (request.url == "/image") {
+      message = "Pagina imagens"
+    } else {
+      message = "Hello world"
+    }
+    response.end(JSON.stringify({
+      "message": message,
+    }))
+  })
   .listen(3000, () => {
     console.log("listening on port 3000");
   });
-
-olaMundo = JSON.stringify({
-  data: "Hello, world",
-});
