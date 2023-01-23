560e456ae300e53dcad2740bf51f7fabb079a8f1
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-01-22 21:55:41 -0300

    feat: adicionando rotas e adicionando objetos

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 const http = require("http");
 
-var message = ""
+var message = "";
+var produto = {
+  preco: Number,
+  nome: String,
+};
+
+var produtos = [
+  {
+    preco: 4,
+    nome: "Boneca",
+  },
+  {
+    preco: 40,
+    nome: "Carro",
+  },
+];
 
 http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "application/json" });
-    if (request.url == "/image") {
-      message = "Pagina imagens"
-    } else {
-      message = "Hello world"
+
+    if (request.url === "/produtos") {
+      response.end(
+        JSON.stringify({
+          produtos: produtos,
+          quantidade: produtos.length,
+        })
+      );
+    } else if (request.url === "/usuario") {
+      response.end(
+        JSON.stringify({
+          usuario: "Samuel Vitor",
+          email: "samuel.vitor@gmail.com",
+        })
+      );
     }
-    response.end(JSON.stringify({
-      "message": message,
-    }))
+
+    response.end(
+      JSON.stringify({
+        message: "Welcome stranger",
+      })
+    );
   })
+
   .listen(3000, () => {
     console.log("listening on port 3000");
   });
