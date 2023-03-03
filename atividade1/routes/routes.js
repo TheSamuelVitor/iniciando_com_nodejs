1c3d5ccb0361eb13053e3a623974389d244fd4f9
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2023-03-03 08:01:08 -0300

    :truck: chore: movendo funcoes para arquivo proprio

--- atividade1/routes/routes.js
+++ atividade1/routes/routes.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const crypto = require("crypto");
-const axios = require("axios");
-const pdfkit = require("pdfkit");
-const fs = require("fs");
+const service = require("../services/services");
 
 const routes = express.Router();
 
 routes.get("/numeros-aleatorios", (req, res) => {
-  let numeros = [];
-
-  for (let index = 0; index < 6; index++) {
-    let numero = crypto.randomInt(60);
-    numeros.push(numero);
-  }
-
+  const numeros = service.numerosAleatorios();
   res.status(200).json({
     numeros: numeros,
   });
@@ -21,49 +12,16 @@ routes.get("/numeros-aleatorios", (req, res) => {
 
 routes.get("/cidades/:letra", async (req, res) => {
   const letra = req.params.letra;
-  let cidades = [];
-  let cidadesNome = [];
-
-  const requisicao = await axios
-    .get("https://servicodados.ibge.gov.br/api/v1/localidades/distritos")
-    .then((response) => {
-      cidades = response.data;
-    });
-
-  cidades.forEach((cidade) => {
-    if (cidade.nome[0] === letra.toUpperCase()) {
-      cidadesNome.push(cidade.nome);
-    }
-  });
-
-  res.json({
+  const cidadesNome = await service.cidadesPorLetra(letra);
+  res.status(200).json({
     cidades: cidadesNome,
   });
 });
 
 routes.get("/estado/:uf/cidades", async (req, res) => {
   const estado = req.params.uf;
-  let estados = [];
-
-  const requisicao = await axios
-    .get(
-      `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/distritos`
-    )
-    .then((response) => {
-      estados = response.data;
-    });
-
-  const pdf = new pdfkit();
-  pdf.text(`Cidades do Estado ${estado}`);
-
-  estados.forEach((estado) => {
-    pdf.text(estado.nome);
-  });
-
-  pdf.end();
-  pdf.pipe(fs.createWriteStream("cidades.pdf")).on("finish", () => {
-    res.status(200).download("./cidades.pdf");
-  });
+  await service.cidadesEstado(estado, res);
+  res.status(200);
 });
 
 module.exports = routes;
---
+++ atividade1/services/services.js
@@ -0,0 +1,58 @@
+const crypto = require("crypto");
+const axios = require("axios");
+const pdfkit = require("pdfkit");
+const fs = require("fs");
+
+exports.numerosAleatorios = function () {
+  let numeros = [];
+
+  for (let index = 0; index < 6; index++) {
+    let numero = crypto.randomInt(60);
+    numeros.push(numero);
+  }
+
+  return numeros;
+};
+
+exports.cidadesPorLetra = async function (letra) {
+  let cidades = [];
+  let cidadesNome = [];
+
+  const requisicao = await axios
+    .get("https://servicodados.ibge.gov.br/api/v1/localidades/distritos")
+    .then((response) => {
+      cidades = response.data;
+    });
+
+  cidades.forEach((cidade) => {
+    if (cidade.nome[0] === letra.toUpperCase()) {
+      cidadesNome.push(cidade.nome);
+    }
+  });
+
+  return cidadesNome;
+};
+
+exports.cidadesEstado = async function (estado, res) {
+  let estados = [];
+
+  const requisicao = await axios
+    .get(
+      `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/distritos`
+    )
+    .then((response) => {
+      estados = response.data;
+    });
+
+  const pdf = new pdfkit();
+  pdf.text(`Cidades do Estado ${estado}`);
+
+  estados.forEach((estado) => {
+    pdf.text(estado.nome);
+  });
+
+  pdf.end();
+  pdf.pipe(fs.createWriteStream("cidades.pdf")).on("finish", () => {
+    res.download("./cidades.pdf");
+  });
+};
