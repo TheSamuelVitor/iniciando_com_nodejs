cc2f8464ece8b180684331b48e32c2dd34344424
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-04-24 22:36:20 -0300

    :wrench: chore: adicionando projeto com typescript

---
+++ setup-node/src/server.ts
@@ -0,0 +1,13 @@
+import Express from "express";
+
+const app = Express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    "mensagem": "pagina principal"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("rodando na porta 3000");
+});
